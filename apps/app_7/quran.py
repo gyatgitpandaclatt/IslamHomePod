846a22dd7fc6c31f409acc5fc9225535c0ade504
gyatgitpandaclatt <167155387+gyatgitpandaclatt@users.noreply.github.com>
2025-02-10 15:01:09 -0800

    directory to play quran

--- apps/app_7/quran.py
+++ apps/app_7/quran.py
@@ -1,1 +1,13 @@
+import pygame
+import os
+
+def play_quran(folder_path):
+    pygame.mixer.init()
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.mp3'):
+            pygame.mixer.music.load(os.path.join(folder_path, filename))
+                pygame.mixer.music.play()
+                while pygame.mixer.music.get_busy():
+                    pygame.time.Clock().tick(10)
+play_quran('/Users/morajaauuuuuuugh/Downloads/IslamHomePod-main/apps/app_7')
 
